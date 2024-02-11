5ca9e56177a1ba7f7ac385c13d11ea6fa1544259
Gabriel Albuquerque <albuquerque.se@proton.me>
2024-02-11 15:06:56 -0300

    refactor(sketchybar): improving structure or colors file

--- sketchybar/.config/sketchybar/colors.sh
+++ sketchybar/.config/sketchybar/colors.sh
@@ -1,45 +1,55 @@
 #!/bin/bash
 
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# COLORS 
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 export WHITE=0xffffffff
 export BLACK=0xf0000000
 
-# -- Teal Scheme --
+
+# -/-
+
+
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# BASE SCHEMES
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+# teal
 export BAR_COLOR=0x0f001f31
 export ITEM_BG_COLOR=0xff003547
 export ACCENT_COLOR=0xff2cf9ed
 
-# -- Gray Scheme --
+# gray
 # export BAR_COLOR=0xff101314
 # export ITEM_BG_COLOR=0xff353c3f
 # export ACCENT_COLOR=0xffffffff
 
-# -- Purple Scheme --
+# purple
 # export BAR_COLOR=0xff140c42
 # export ITEM_BG_COLOR=0xff2b1c84
 # export ACCENT_COLOR=0xffeb46f9
 
-# -- Red Scheme ---
+# red
 # export BAR_COLOR=0xff23090e
 # export ITEM_BG_COLOR=0xff591221
 # export ACCENT_COLOR=0xffff2453
 
-# -- Blue Scheme --- 
+# blue
 # export BAR_COLOR=0xff021254
 # export ITEM_BG_COLOR=0xff093aa8
 # export ACCENT_COLOR=0xff15bdf9
 
-# -- Green Scheme --
+# green
 # export BAR_COLOR=0xff003315
 # export ITEM_BG_COLOR=0xff008c39
 # export ACCENT_COLOR=0xff1dfca1
 
-
-# -- Orange Scheme --
+# orange
 # export BAR_COLOR=0xff381c02
 # export ITEM_BG_COLOR=0xff99440a
 # export ACCENT_COLOR=0xfff97716
 
-# -- Yellow Scheme --
+# yellow
 # export BAR_COLOR=0xff2d2b02
 # export ITEM_BG_COLOR=0xff8e7e0a
 # export ACCENT_COLOR=0xfff7fc17

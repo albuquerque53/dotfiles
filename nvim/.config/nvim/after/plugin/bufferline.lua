d92a26fd8fe39072970e47365b472bd7ac1f4e38
Gabriel Albuquerque <albuquerque.se@proton.me>
2024-02-21 20:41:19 -0300

    fix(nvim): bufferline bug

--- nvim/.config/nvim/after/plugin/bufferline.lua
+++ nvim/.config/nvim/after/plugin/bufferline.lua
@@ -1,9 +1,2 @@
 -- Bufferline confs
-
-require("bufferline").setup{
-    options = {
-        always_show_bufferline = false,
-        show_close_icon = false,
-        color_icons = true
-    }
-}
+require("bufferline").setup{}

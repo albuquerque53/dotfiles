b338a4f985877a9e9e56b8db91c5c6ddbe0991ea
Gabriel Albuquerque <albuquerque.se@proton.me>
2024-02-21 19:52:49 -0300

    feat(nvim): using a custom neovim startup

--- nvim/.config/nvim/after/plugin/startup.lua
+++ nvim/.config/nvim/after/plugin/startup.lua
@@ -1,1 +1,10 @@
-require("startup").setup({theme = "dashboard"})
+require("startup").setup({
+    theme = "minimal_evil",
+    mappings = {
+        execute_command = "<CR>",
+        open_file = "o",
+        open_file_split = "<c-o>",
+        open_section = "<ENTER>",
+        open_help = "?",
+    },
+})

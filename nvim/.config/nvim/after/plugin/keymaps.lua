00e4c2e145987078ea54a8163da089d64256f55b
Gabriel Albuquerque <albuquerque.se@proton.me>
2024-03-04 09:29:00 -0300

    feat(nvim): buffers bind

--- nvim/.config/nvim/after/plugin/keymaps.lua
+++ nvim/.config/nvim/after/plugin/keymaps.lua
@@ -9,6 +9,7 @@ vim.api.nvim_set_keymap("n", "<Space>ff", ":Telescope find_files<cr>", { noremap
 vim.api.nvim_set_keymap("n", "<Space>fg", ":Telescope live_grep<cr>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<Space>fb", ":Telescope file_browser path=%:p:h select_buffer=true<cr>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<Space>fh", ":Telescope help_tags<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Space>sb", ":Telescope buffers<cr>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<Space>cl", ":bd<cr>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<Space>ca", ":bufdo bd<cr>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<Space>pps", ":Peepsight<cr>", { noremap = true, silent = true })

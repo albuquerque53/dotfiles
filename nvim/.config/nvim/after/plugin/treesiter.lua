5e83bbe7711b773425a8c3971b07dd746db242ee
Gabriel Albuquerque <albuquerque.se@proton.me>
2024-03-13 17:53:31 -0300

    feat(nvim): adding more syntax support for blade files

--- nvim/.config/nvim/after/plugin/treesiter.lua
+++ nvim/.config/nvim/after/plugin/treesiter.lua
@@ -1,5 +1,21 @@
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.blade = {
+  install_info = {
+    url = "https://github.com/EmranMR/tree-sitter-blade",
+    files = {"src/parser.c"},
+    branch = "main",
+  },
+  filetype = "blade"
+}
+
+vim.api.nvim_exec([[
+  augroup BladeFiltypeRelated
+    au BufNewFile,BufRead *.blade.php set ft=blade
+  augroup END
+]], false)
+
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "php", "lua", "vim", "html", "go" },
+  ensure_installed = { "php", "lua", "vim", "html", "go", "blade" },
   sync_install = false,
   auto_install = true
 }
